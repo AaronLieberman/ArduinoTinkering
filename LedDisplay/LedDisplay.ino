3895d4256dde25b37b7a021b8d249809672817dc
Aaron Lieberman <aaronlcode@gmail.com>
2015-08-16 16:22:22 -0700

    updates for new board layout, wip debugging stuff

--- LedDisplay/LedDisplay.ino
+++ LedDisplay/LedDisplay.ino
@@ -1,25 +1,29 @@
 #include <arduino.h>
 
+// led on device
+const int ledPin = 13;
+const int hackPin = 8;
+
 // shared pins
-int outputDisabledPin = 9;
-int registerClockPin = 10;
+const int outputDisabledPin = 3;
+const int registerClockPin = 11;
 
 // segment pins
-int whichSegmentData = 2; 
-int segmentShiftRegisterClockPin = 4;
+const int whichSegmentData = 9; 
+const int segmentShiftRegisterClockPin = 10;
 
 // digit pins
-int whichDigitData = 11;
-int shiftRegisterClockPin = 12;
+const int whichDigitData = 6;
+const int digitShiftRegisterClockPin = 5;
 
-const int segTop = (1 << 0);
+const int segTop = (1 << 7);
 const int segDot = (1 << 1);
-const int segMiddle = (1 << 2);
-const int segBottomRight = (1 << 3);
-const int segBottomLeft = (1 << 4);
-const int segBottom = (1 << 5);
+const int segMiddle = (1 << 5);
+const int segBottomRight = (1 << 4);
+const int segBottomLeft = (1 << 2);
+const int segBottom = (1 << 3);
 const int segTopLeft = (1 << 6);
-const int segTopRight = (1 << 7);
+const int segTopRight = (1 << 0);
 
 byte segmentIndex = 0;
 int valueToDisplay = 0;
@@ -42,25 +46,60 @@ int digitValues[] =
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println();
-  
+  //Serial.begin(9600);
+  //Serial.println();
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(hackPin, OUTPUT);
   pinMode(outputDisabledPin, OUTPUT);
   pinMode(registerClockPin, OUTPUT);  
   pinMode(whichSegmentData, OUTPUT);
   pinMode(segmentShiftRegisterClockPin, OUTPUT);  
   pinMode(whichDigitData, OUTPUT);
-  pinMode(shiftRegisterClockPin, OUTPUT);
+  pinMode(digitShiftRegisterClockPin, OUTPUT);
 
   digitalWrite(outputDisabledPin, LOW);
+  digitalWrite(registerClockPin, LOW);
+  digitalWrite(whichSegmentData, LOW);
+  digitalWrite(segmentShiftRegisterClockPin, LOW);
+  digitalWrite(whichDigitData, LOW);
+  digitalWrite(digitShiftRegisterClockPin, LOW);
+  
+  //shiftOut(whichSegmentData, segmentShiftRegisterClockPin, MSBFIRST, 255);
+  //shiftOut(whichDigitData, digitShiftRegisterClockPin, MSBFIRST, 0);
+  //digitalWrite(registerClockPin, HIGH);
+  //digitalWrite(registerClockPin, LOW);
 
-  writeValue(0, 0);
-  valueToDisplay = 0;
+  valueToDisplay = 1234;
+  writeValue(0, valueToDisplay);
+}
+
+bool toggle = false;
+unsigned long lastToggle = 0;
+
+void loop2()
+{
+  unsigned long now = millis();
+
+  if (now - lastToggle > 3000)
+  {
+    toggle = !toggle;
+    lastToggle = now;
+  }
+
+  unsigned long pinValue = toggle ? HIGH : LOW;
+  digitalWrite(ledPin, pinValue);
+  digitalWrite(hackPin, pinValue);
+  //digitalWrite(registerClockPin, pinValue);
+  //digitalWrite(whichSegmentData, pinValue);
+  //digitalWrite(segmentShiftRegisterClockPin, pinValue);
+  //digitalWrite(whichDigitData, pinValue);
+  //digitalWrite(digitShiftRegisterClockPin, pinValue);
 }
 
 void loop()
 {
-  // Arduino pow function is approximage and sometimes will end up rounding down when casting to int
+  // Arduino pow function is approximate and sometimes will end up rounding down when casting to int
   int place = pow(10, 3 - segmentIndex) + 0.5f;
   int segmentValue = (valueToDisplay / place) % 10;
 
@@ -71,9 +110,11 @@ void loop()
   unsigned long now = millis();
   if (now > lastIncrement + 20)
   {
-    valueToDisplay++;
+    valueToDisplay = (valueToDisplay + 1) % 10000;
     lastIncrement = now;
   }
+
+  digitalWrite(ledPin, (now % 200 == 0) ? HIGH : LOW);
 }
 
 void writeValue(byte digit, byte value)
@@ -82,7 +123,7 @@ void writeValue(byte digit, byte value)
   int segmentData = digitValues[value];
   
   shiftOut(whichSegmentData, segmentShiftRegisterClockPin, MSBFIRST, segmentData);
-  shiftOut(whichDigitData, shiftRegisterClockPin, MSBFIRST, digitData);
+  shiftOut(whichDigitData, digitShiftRegisterClockPin, MSBFIRST, digitData);
 
   digitalWrite(registerClockPin, HIGH);
   digitalWrite(registerClockPin, LOW);
