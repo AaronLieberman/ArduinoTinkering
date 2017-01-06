8680610bb7c6fbb8b7f9ea5580095198f3feb1dd
Aaron Lieberman <aaronlcode@gmail.com>
2017-01-05 22:55:02 -0800

    Simple sketch to blink neopixels for halloween

--- Halloween/Halloween.ino
+++ Halloween/Halloween.ino
@@ -10,8 +10,11 @@
 // On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
 
+#define STRANDLENGTH 7
+#define NUMSTRANDS 4
+
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      28
+#define NUMPIXELS STRANDLENGTH * NUMSTRANDS
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -51,15 +54,121 @@ void setPixels(Color3F c, int from, int to)
   pixels.show();
 }
 
+float hue = 0;
+const float hueOffset = 0.01f;
+float phase = 0;
+const float phaseOffset = 1.0f / 167;
+
+Color3F GenerateHue(float hue)
+{
+  float u = cos(hue * 2 * M_PI);
+  float w = sin(hue * 2 * M_PI);
+
+  Color3F ret;
+  float s = 255;
+
+  float third = 1.0f / 3;
+  ret.R = (cos(hue * 2 * M_PI) / 2 + 0.5f) * s;
+  ret.G = (cos((hue + third) * 2 * M_PI) / 2 + 0.5f) * s;
+  ret.B = (cos((hue + 2 * third) * 2 * M_PI) / 2 + 0.5f) * s;
+
+  return ret;
+}
+
+void patternHue()
+{
+  for (int j = 0; j < 700; j++)
+  {
+    for(int i = 0; i < NUMPIXELS; i++)
+    {
+      Color3F c = GenerateHue(hue + ((float)i) / NUMPIXELS);
+  
+      float brightness = cos((phase + 1.0f * i / NUMPIXELS) * 2 * M_PI) / 2 + 0.5f;
+      pixels.setPixelColor(i, pixels.Color(c.R * brightness, c.G * brightness, c.B * brightness));
+  
+      pixels.show(); // This sends the updated pixel color to the hardware.
+    }
+  
+    delay(1); // Delay for a period of time (in milliseconds).
+  
+    hue = hue + hueOffset >= 1 ? 0 : hue + hueOffset;
+    phase = phase + phaseOffset >= 1 ? 0 : phase + phaseOffset;
+  }
+}
+
+void patternBlink()
+{
+  for (int i = 0; i < 7  ; i++)
+  {
+    setPixels(yellow, 0, NUMPIXELS);
+    delay(1000);
+    setPixels(black, 0, NUMPIXELS);
+    delay(100);
+    setPixels(red, 0, NUMPIXELS);
+    delay(100);
+    setPixels(black, 0, NUMPIXELS);
+    delay(100);
+  }
+}
+
+void patternFlicker()
+{
+  for (int i = 0; i < 7; i++)
+  {
+    setPixels(yellow, 0, NUMPIXELS);
+    delay(2000 + random(10) * 1000);
+
+    setPixels(black, 0, NUMPIXELS);
+    delay(50);
+    setPixels(yellow, 0, NUMPIXELS);
+    delay(100);
+    setPixels(black, 0, NUMPIXELS);
+    delay(50);
+  }
+}
+
+void patternParade()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < NUMSTRANDS; j++)
+    {
+      setPixels(black, 0, NUMPIXELS);
+      setPixels(white, j * STRANDLENGTH, (j + 1) * STRANDLENGTH);
+      delay(200);
+    }
+  }
+}
+
+void patternParadeAndBack()
+{
+  for (int i = 0; i < 7; i++)
+  {
+    for (int j = 1; j < NUMSTRANDS; j++)
+    {
+      setPixels(black, 0, NUMPIXELS);
+      setPixels(white, j * STRANDLENGTH, (j + 1) * STRANDLENGTH);
+      delay(200);
+    }
+
+    for (int j = NUMSTRANDS - 2; j >= 0; j--)
+    {
+      setPixels(black, 0, NUMPIXELS);
+      setPixels(white, j * STRANDLENGTH, (j + 1) * STRANDLENGTH);
+      delay(200);
+    }
+  }
+}
+
 void loop()
 {
-  setPixels(yellow, 0, NUMPIXELS);
-  delay(1000);
-  setPixels(black, 0, NUMPIXELS);
-  delay(100);
-  setPixels(red, 0, NUMPIXELS);
-  delay(100);
-  setPixels(black, 0, NUMPIXELS);
-  delay(100);
+  switch (random(0, 5))
+  {
+    case 0: patternBlink(); break;
+    case 1: patternFlicker(); break;
+    case 2: patternParade(); break;
+    case 3: patternParadeAndBack(); break;
+    case 4: patternHue(); break;
+  }
 }
 
