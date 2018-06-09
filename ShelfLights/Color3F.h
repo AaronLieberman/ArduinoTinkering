42963c9b54e4d379249e3ad2722755bb18abfc6e
Aaron Lieberman <aaronlcode@gmail.com>
2018-06-08 20:55:06 -0700

    clang format

--- ShelfLights/Color3F.h
+++ ShelfLights/Color3F.h
@@ -4,22 +4,16 @@
 class Color3F
 {
 public:
+	float R;
+	float G;
+	float B;
 
-  float R;
-  float G;
-  float B;
+	Color3F() : R(0), G(0), B(0) {}
 
-  Color3F() : R(0), G(0), B(0)
-  {
-  }
-
-  Color3F(float r, float g, float b)
-  {
-    R = r;
-    G = g;
-    B = b;
-  }
+	Color3F(float r, float g, float b)
+	{
+		R = r;
+		G = g;
+		B = b;
+	}
 };
-
-
-
