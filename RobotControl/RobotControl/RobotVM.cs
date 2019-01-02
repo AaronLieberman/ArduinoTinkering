03de2c9c9dc22fe66ab778323bc1ef2049d31c7c
Aaron Lieberman <aaronlcode@gmail.com>
2019-01-01 21:12:08 -0800

    Add message received UI

--- RobotControl/RobotControl/RobotVM.cs
+++ RobotControl/RobotControl/RobotVM.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
 using Addle.Wpf;
 using Addle.Wpf.ViewModel;
 
@@ -12,27 +14,49 @@ namespace RobotControl
     {
         readonly List<ServoVM> _servos = new List<ServoVM>();
         readonly ServoManager _servoManager;
-        readonly ActionCommand _connectCommand;
+        readonly ObservableCollection<string> _receivedMessages = new ObservableCollection<string>();
 
         public RobotVM(ServoManager servoManager)
         {
             _servoManager = servoManager;
-            var servos = servoManager.Servos;
-            foreach (var servo in servos)
+
+            foreach (var servo in servoManager.Servos)
             {
                 _servos.Add(new ServoVM(servo.Name, v => servoManager.SetServoPosition(servo.ServoIndex, v)));
             }
 
-            _connectCommand = new ActionCommand(() => _servoManager.Start(_serialPortName));
+            Connect = new ActionCommand(() => _servoManager.Start(_serialPortName));
+            Clear = new ActionCommand(() => _receivedMessages.Clear());
+
+            _servoManager.MessageReceived.ObserveOnDispatcher().Subscribe(s => MessageReceived(s));
         }
 
         public IEnumerable<ServoVM> Servos { get { return _servos; } }
-
+        public IEnumerable<string> ReceivedMessages { get { return _receivedMessages; } }
+        
         #region string SerialPortName (VM property)
         string _serialPortName = "COM6";
         public string SerialPortName { get { return _serialPortName; } set { SetProperty(ref _serialPortName, value); } }
         #endregion
 
-        public ActionCommand Connect { get { return _connectCommand; } }
+        #region string ReceivedMessageIndex (VM property)
+        int _receivedMessageIndex = -1;
+        public int ReceivedMessageIndex { get { return _receivedMessageIndex; } set { SetProperty(ref _receivedMessageIndex, value); } }
+        #endregion
+
+        public ActionCommand Connect { get; }
+        public ActionCommand Clear { get; }
+
+        void MessageReceived(string message)
+        {
+            _receivedMessages.Add(message);
+            _receivedMessageIndex = _receivedMessages.Count - 1;
+        }
+
+        void ClearMessages(string message)
+        {
+            _receivedMessages.Clear();
+            _receivedMessageIndex = -1;
+        }
     }
 }
