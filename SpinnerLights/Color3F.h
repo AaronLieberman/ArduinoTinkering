605278db73b78a85ffef41b8a736b5d1f9a3e5d0
Aaron Lieberman <aaronlcode@gmail.com>
2021-08-23 17:15:02 -0700

    SpinnerLights: uses accelerometer on PyBadge to control neopixels

---
+++ SpinnerLights/Color3F.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <Arduino.h>
+
+class Color3F
+{
+public:
+	float R;
+	float G;
+	float B;
+
+	Color3F() : R(0), G(0), B(0) {}
+
+	Color3F(float r, float g, float b)
+	{
+		R = r;
+		G = g;
+		B = b;
+	}
+
+	Color3F operator*(float scalar) const
+	{
+		Color3F result = *this;
+		result.R *= scalar;
+		result.G *= scalar;
+		result.B *= scalar;
+		return result;
+	}
+
+	static Color3F FromBytes(byte r, byte g, byte b)
+	{
+		Color3F result;
+		result.R = float(r) / 255;
+		result.G = float(g) / 255;
+		result.B = float(b) / 255;
+		return result;
+	}
+};
--- SpinnerLights/SpinnerLights.ino
+++ SpinnerLights/SpinnerLights.ino
@@ -1,22 +1,24 @@
-//#include "Color3F.h"
-
-#include "SerialPrintf.h"
-
-//#include "stlhelper.h" // includes Arduino.h
 #include <Arduino.h>
 
+#include <Adafruit_LIS3DH.h>
 #include <Adafruit_NeoPixel.h>
+
+#include "Color3F.h"
+#include "SerialPrintf.h"
 
 #include <functional>
 #include <memory>
 #include <string>
 #include <vector>
 
-const int kNeoPixelPin = 1;
+const int kNeoPixelPin = 14;
 const int kLedPin = 13;
 const int kNumNeoPixels = 3;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(kNumNeoPixels, kNeoPixelPin, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(kNumNeoPixels, kNeoPixelPin, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel strip(5, 8, NEO_GRB + NEO_KHZ800); // 5 NeoPixels are on D8
+
+Adafruit_LIS3DH accel;
 
 void setup() {
 	Serial.begin(115200);
@@ -27,29 +29,42 @@ void setup() {
 	}
 #endif
 
+	pixels.begin();
+	strip.begin();
+
+	if (!accel.begin(0x18) && !accel.begin(0x19)) {
+		Serial.print("Failed to find accelerometer");
+		strip.fill(0xff0000);
+		strip.show();
+		while (true) {
+		}
+	}
+
+	accel.setRange(LIS3DH_RANGE_4_G); // 2, 4, 8 or 16 G
+
+	strip.show();
+
 	pinMode(kLedPin, OUTPUT);
 
-	pixels.begin();
 	pixels.show();
 
 	delay(10);
 }
 
+uint8_t valueFromGravity(float v) {
+	v = abs(v);
+	v = v > 9.8 ? 9.8 : v;
+	v = v / 9.8;
+	return (uint8_t)(pow(v, 4) * 255);
+}
+
 void loop() {
-	// digitalWrite(kLedPin, LOW);
-	// delay(300);
-	// digitalWrite(kLedPin, HIGH);
-	// delay(150);
+	sensors_event_t event;
+	accel.getEvent(&event);
 
-	static int foo = 0;
-	foo++;
+	pixels.setPixelColor(0, valueFromGravity(event.acceleration.x) << 16);
+	pixels.setPixelColor(1, valueFromGravity(event.acceleration.y) << 8);
+	pixels.setPixelColor(2, valueFromGravity(event.acceleration.z));
 
-	for (int i = 0; i < kNumNeoPixels; i++) {
-		int v = foo % kNumNeoPixels == i ? 255 : 0;
-		pixels.setPixelColor(i, v, 0, 0);
-	}
 	pixels.show();
-
-	digitalWrite(kLedPin, (foo % 2) == 0 ? LOW : HIGH);
-	delay(100);
 }
