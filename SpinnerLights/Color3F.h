55036b75d3be9a18f77fa8a0c2d20debf97a30ea
Aaron Lieberman <aaronlcode@gmail.com>
2021-08-27 17:26:44 -0700

    A little header cleanup

--- SpinnerLights/Color3F.h
+++ SpinnerLights/Color3F.h
@@ -1,8 +1,8 @@
 #pragma once
+
 #include <Arduino.h>
 
-class Color3F
-{
+class Color3F {
 public:
 	float R;
 	float G;
@@ -10,15 +10,13 @@ public:
 
 	Color3F() : R(0), G(0), B(0) {}
 
-	Color3F(float r, float g, float b)
-	{
+	Color3F(float r, float g, float b) {
 		R = r;
 		G = g;
 		B = b;
 	}
 
-	Color3F operator*(float scalar) const
-	{
+	Color3F operator*(float scalar) const {
 		Color3F result = *this;
 		result.R *= scalar;
 		result.G *= scalar;
@@ -26,8 +24,7 @@ public:
 		return result;
 	}
 
-	static Color3F FromBytes(byte r, byte g, byte b)
-	{
+	static Color3F FromBytes(byte r, byte g, byte b) {
 		Color3F result;
 		result.R = float(r) / 255;
 		result.G = float(g) / 255;
