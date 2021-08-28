f6481147c32554c02bf912011e56fbaa884e554d
Aaron Lieberman <aaronlcode@gmail.com>
2021-08-27 17:23:52 -0700

    Filter accel values, a bit of cleanup

--- SpinnerLights/SpinnerLights.ino
+++ SpinnerLights/SpinnerLights.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_NeoPixel.h>
 
+#include "AveragingFilter.h"
 #include "Color3F.h"
 #include "SerialPrintf.h"
 
@@ -15,10 +16,14 @@ const int kNeoPixelPin = 14;
 const int kLedPin = 13;
 const int kNumNeoPixels = 3;
 
-Adafruit_NeoPixel pixels(kNumNeoPixels, kNeoPixelPin, NEO_RGB + NEO_KHZ800);
-Adafruit_NeoPixel strip(5, 8, NEO_GRB + NEO_KHZ800); // 5 NeoPixels are on D8
+Adafruit_NeoPixel _pixels(kNumNeoPixels, kNeoPixelPin, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel _strip(5, 8, NEO_GRB + NEO_KHZ800); // 5 NeoPixels are on D8
 
-Adafruit_LIS3DH accel;
+Adafruit_LIS3DH _accel;
+
+AveragingFilter _filterX(10);
+AveragingFilter _filterY(10);
+AveragingFilter _filterZ(10);
 
 void setup() {
 	Serial.begin(115200);
@@ -29,24 +34,23 @@ void setup() {
 	}
 #endif
 
-	pixels.begin();
-	strip.begin();
+	_pixels.begin();
+	_strip.begin();
 
-	if (!accel.begin(0x18) && !accel.begin(0x19)) {
+	if (!_accel.begin(0x18) && !_accel.begin(0x19)) {
 		Serial.print("Failed to find accelerometer");
-		strip.fill(0xff0000);
-		strip.show();
+		_strip.fill(0xff0000);
+		_strip.show();
 		while (true) {
 		}
 	}
 
-	accel.setRange(LIS3DH_RANGE_4_G); // 2, 4, 8 or 16 G
+	_accel.setRange(LIS3DH_RANGE_4_G); // 2, 4, 8 or 16 G
 
-	strip.show();
+	_pixels.show();
+	_strip.show();
 
 	pinMode(kLedPin, OUTPUT);
-
-	pixels.show();
 
 	delay(10);
 }
@@ -55,16 +59,17 @@ uint8_t valueFromGravity(float v) {
 	v = abs(v);
 	v = v > 9.8 ? 9.8 : v;
 	v = v / 9.8;
-	return (uint8_t)(pow(v, 4) * 255);
+	return (uint8_t)(pow(v, 2) * 255);
 }
 
 void loop() {
 	sensors_event_t event;
-	accel.getEvent(&event);
+	_accel.getEvent(&event);
 
-	pixels.setPixelColor(0, valueFromGravity(event.acceleration.x) << 16);
-	pixels.setPixelColor(1, valueFromGravity(event.acceleration.y) << 8);
-	pixels.setPixelColor(2, valueFromGravity(event.acceleration.z));
+	_pixels.setPixelColor(0, valueFromGravity(_filterX.addSample(event.acceleration.x)) << 16);
+	_pixels.setPixelColor(1, valueFromGravity(_filterY.addSample(event.acceleration.y)) << 8);
+	_pixels.setPixelColor(2, valueFromGravity(_filterZ.addSample(event.acceleration.z)));
 
-	pixels.show();
+	_pixels.show();
+	delay(10);
 }
