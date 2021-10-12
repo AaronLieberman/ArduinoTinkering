3c9578ca84d6b0f3494f5fc1f8a2e2af65540300
Aaron Lieberman <aaronlcode@gmail.com>
2021-10-12 08:08:11 -0700

    Basic spinner works

--- SpinnerLights/SpinnerLights.ino
+++ SpinnerLights/SpinnerLights.ino
@@ -3,7 +3,6 @@
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_NeoPixel.h>
 
-#include "AveragingFilter.h"
 #include "Color3F.h"
 #include "SerialPrintf.h"
 
@@ -12,18 +11,23 @@
 #include <string>
 #include <vector>
 
-const int kNeoPixelPin = 14;
+//#define CIRCUITPLAYGROUND
+
+#ifdef CIRCUITPLAYGROUND
+const int kNeoPixelPin = 3;
+#else
+const int kNeoPixelPin = 1;
+#endif
 const int kLedPin = 13;
-const int kNumNeoPixels = 3;
+const int kNumNeoPixels = 1;
 
 Adafruit_NeoPixel _pixels(kNumNeoPixels, kNeoPixelPin, NEO_RGB + NEO_KHZ800);
+
+#ifdef CIRCUITPLAYGROUND
 Adafruit_NeoPixel _strip(5, 8, NEO_GRB + NEO_KHZ800); // 5 NeoPixels are on D8
+#endif
 
 Adafruit_LIS3DH _accel;
-
-AveragingFilter _filterX(10);
-AveragingFilter _filterY(10);
-AveragingFilter _filterZ(10);
 
 void setup() {
 	Serial.begin(115200);
@@ -35,20 +39,26 @@ void setup() {
 #endif
 
 	_pixels.begin();
+#ifdef CIRCUITPLAYGROUND
 	_strip.begin();
+#endif
 
 	if (!_accel.begin(0x18) && !_accel.begin(0x19)) {
 		Serial.print("Failed to find accelerometer");
+#ifdef CIRCUITPLAYGROUND
 		_strip.fill(0xff0000);
 		_strip.show();
+#endif
 		while (true) {
 		}
 	}
 
-	_accel.setRange(LIS3DH_RANGE_4_G); // 2, 4, 8 or 16 G
+	_accel.setRange(LIS3DH_RANGE_2_G); // 2, 4, 8 or 16 G
 
 	_pixels.show();
+#ifdef CIRCUITPLAYGROUND
 	_strip.show();
+#endif
 
 	pinMode(kLedPin, OUTPUT);
 
@@ -66,9 +76,17 @@ void loop() {
 	sensors_event_t event;
 	_accel.getEvent(&event);
 
-	_pixels.setPixelColor(0, valueFromGravity(_filterX.addSample(event.acceleration.x)) << 16);
-	_pixels.setPixelColor(1, valueFromGravity(_filterY.addSample(event.acceleration.y)) << 8);
-	_pixels.setPixelColor(2, valueFromGravity(_filterZ.addSample(event.acceleration.z)));
+#if MODE == 1
+	_pixels.setPixelColor(0, valueFromGravity(event.acceleration.x) << 16);
+	_pixels.setPixelColor(1, valueFromGravity(event.acceleration.y) << 8);
+	_pixels.setPixelColor(2, valueFromGravity(event.acceleration.z));
+#else
+	float yAccel = event.acceleration.y;
+	uint8_t v = yAccel < -13 ? 255 : 0;
+	for (int i = 0; i < kNumNeoPixels; i++) {
+		_pixels.setPixelColor(i, v, v * 0.5f, 0);
+	}
+#endif
 
 	_pixels.show();
 	delay(10);
